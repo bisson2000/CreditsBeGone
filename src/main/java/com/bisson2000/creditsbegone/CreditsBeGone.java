de607d2c604e1f0a90770d47edb71016c33776cd
Paul-Andre Bisson <bissonpaulandre@gmail.com>
2025-02-17 08:30:25 -0500

    initial commit

---
+++ src/main/java/com/bisson2000/creditsbegone/CreditsBeGone.java
@@ -0,0 +1,32 @@
+package com.bisson2000.creditsbegone;
+
+import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+
+// The value here should match an entry in the META-INF/mods.toml file
+@Mod(CreditsBeGone.MOD_ID)
+public class CreditsBeGone
+{
+    // Define mod id in a common place for everything to reference
+    public static final String MOD_ID = "creditsbegone";
+
+    public CreditsBeGone(FMLJavaModLoadingContext context)
+    {
+    }
+
+    // You can use EventBusSubscriber to automatically register all static methods in the class annotated with @SubscribeEvent
+    @Mod.EventBusSubscriber(modid = MOD_ID, bus = Mod.EventBusSubscriber.Bus.MOD, value = Dist.CLIENT)
+    public static class ClientModEvents
+    {
+        @SubscribeEvent
+        public static void onClientSetup(FMLClientSetupEvent event)
+        {
+            // Some client setup code
+            //LOGGER.info("HELLO FROM CLIENT SETUP");
+            //LOGGER.info("MINECRAFT NAME >> {}", Minecraft.getInstance().getUser().getName());
+        }
+    }
+}
